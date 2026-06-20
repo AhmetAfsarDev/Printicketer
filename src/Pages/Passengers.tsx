089323a365ae592f29e6845b636be4b2f047d227
Ahmet <ahmetilk0510@gmail.com>
2026-06-20 17:42:05 +0300

    Update Passengers.tsx

--- src/Pages/Passengers.tsx
+++ src/Pages/Passengers.tsx
@@ -3,23 +3,32 @@ import "../App.css"
 
 function Passengers() {
   return (
+    
     <div className='Passengers'>
+<section className='Header' >
+            <div className="logowrapper"><img src='../public/Logo.png' /></div>
+            <ul>
+              <li><a href="#">Müşteriler</a></li>
+              <li><a href="#">Seferler</a></li>
+              <li><a href="#">Sohbet</a></li>
+            </ul>
+        </section>
 
-        <div className="pessengerheader">
-        <span>Bilet listesi</span>
-        <button>X</button>
-        </div>
+        
 
         <div className='ticketcard'>
          <div className='Passengerdatabox'>
-                 <span>Yolcu adı: <span className='Passengername'>Yolcuadı</span></span>
-             <span>Yolcu soyadı: <span className='Passengersurname'>Yolcu soyadı</span></span>
-               <span>Nereden: <span className='Where'>Binilen şehir</span></span>
-             <span>Nereye: <span className='To-where'>Gidilen şehir</span></span>
-             <span>Yolculuk tarihi: <span className='Date'>Tarih</span></span>
+               <h3>Yolcu bilgileri</h3>
+              <span>Yolcu adı: <span className='Passengername'>Yolcuadı</span></span>
+              <span>Yolcu soyadı: <span className='Passengersurname'>Yolcu soyadı</span></span>
+              <span>Nereden: <span className='Where'>Binilen şehir</span></span>
+              <span>Nereye: <span className='To-where'>Gidilen şehir</span></span>
+              <span>Yolculuk tarihi: <span className='Date'>Tarih</span></span>
          </div>
         </div>
 
+          
+      
         
     </div>
   )
